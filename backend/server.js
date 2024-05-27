1dc7a96f98daadbb5f7fa8b0a077334f91bbd4a7
dardanhajdaraj <dh49980@ubt-uni.net>
2024-05-27 09:12:22 +0200

    create post rout-backend and connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,30 @@
+require("dotenv").config();
 const express = require("express");
+const prompt = require('prompt-sync');
+const Post = require("./models/Post/Post");
+const connectDB = require("./utils/connectDB");
+
+connectDB();
 
 const app = express();
 
 const PORT = 5000;
+
+app.use(express.json());
+
+app.post('/api/v1/posts/create', async(req, res)=>{
+    try{
+        const postData = req.body;
+        const postCreated = await Post.create(postData);
+        res.json({
+            status: "success",
+            message: "Post created successfully",
+            postCreated,
+        });
+    }catch(error){
+        console.log(error);
+        res.json(error);
+    }
+});
+
 app.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
